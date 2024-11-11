c9aa71f4146a16ba47bb7dfd1abf5ec4a09e5b0a
abdurrehman107 <razashahid107@gmail.com>
2024-11-11 15:50:15 +0500

    minor

--- internal/webhook/v1/maintenancewindow_webhook.go
+++ internal/webhook/v1/maintenancewindow_webhook.go
@@ -45,7 +45,7 @@ func SetupMaintenanceWindowWebhookWithManager(mgr ctrl.Manager) error {
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-// +kubebuilder:webhook:path=/mutate-maintenance-custom-io-maintenence-window-io-v1-maintenancewindow,mutating=true,failurePolicy=fail,sideEffects=None,groups=maintenance.custom.io.maintenence-window.io,resources=maintenancewindows,verbs=create;update,versions=v1,name=mmaintenancewindow-v1.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-maintenance-custom-io-maintenance-window-io-v1-maintenancewindow,mutating=true,failurePolicy=fail,sideEffects=None,groups=maintenance.custom.io.maintenance-window.io,resources=maintenancewindows,verbs=create;update,versions=v1,name=mmaintenancewindow-v1.kb.io,admissionReviewVersions=v1
 
 // MaintenanceWindowCustomDefaulter struct is responsible for setting default values on the custom resource of the
 // Kind MaintenanceWindow when those are created or updated.
@@ -76,7 +76,7 @@ func (d *MaintenanceWindowCustomDefaulter) Default(ctx context.Context, obj runt
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-maintenance-custom-io-maintenence-window-io-v1-maintenancewindow,mutating=false,failurePolicy=fail,sideEffects=None,groups=maintenance.custom.io.maintenence-window.io,resources=maintenancewindows,verbs=create;update,versions=v1,name=vmaintenancewindow-v1.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-maintenance-custom-io-maintenance-window-io-v1-maintenancewindow,mutating=false,failurePolicy=fail,sideEffects=None,groups=maintenance.custom.io.maintenance-window.io,resources=maintenancewindows,verbs=create;update,versions=v1,name=vmaintenancewindow-v1.kb.io,admissionReviewVersions=v1
 
 // MaintenanceWindowCustomValidator struct is responsible for validating the MaintenanceWindow resource
 // when it is created, updated, or deleted.
