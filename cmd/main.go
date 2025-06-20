711842b897f5f70e36725453d9979dc3a9bc05c1
Abdur Rehman <razashahid107@gmail.com>
2025-06-20 21:27:57 +0500

    chore: add working webhook config

--- cmd/main.go
+++ cmd/main.go
@@ -125,7 +125,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "4cc53dd0.github.com/abdurrehman107",
+		LeaderElectionID:       "maintenance-window-leader-election",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
